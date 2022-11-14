573b74d855b5f0ded42be952f357061256fa310e
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2022-11-14 15:04:40 +0900

    ウインドウのサイズ変更と，矢印キーでの操作の実装．

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
@@ -43,18 +43,22 @@ class Ball extends Shape {
     }
     update() {
         if ((this.x + this.size) >= width) {
+            this.x = width - this.size;
             this.velX = -(this.velX);
         }
 
         if ((this.x - this.size) <= 0) {
+            this.x = this.size;
             this.velX = -(this.velX);
         }
 
         if ((this.y + this.size) >= height) {
+            this.y = height - this.size;
             this.velY = -(this.velY);
         }
 
         if ((this.y - this.size) <= 0) {
+            this.y = this.size;
             this.velY = -(this.velY);
         }
 
@@ -84,15 +88,23 @@ class EvilCircle extends Shape {
         this.size = 10;
         window.addEventListener('keydown', (e) => {
             switch (e.key) {
+                case "Left": // IE/Edge specific value
+                case "ArrowLeft":
                 case 'a':
                     this.x -= this.velX;
                     break;
+                case "Right": // IE/Edge specific value
+                case "ArrowRight":
                 case 'd':
                     this.x += this.velX;
                     break;
+                case "Up": // IE/Edge specific value
+                case "ArrowUp":
                 case 'w':
                     this.y -= this.velY;
                     break;
+                case "Down": // IE/Edge specific value
+                case "ArrowDown":
                 case 's':
                     this.y += this.velY;
                     break;
@@ -158,15 +170,22 @@ while (balls.length < 25) {
 let evilBall = new EvilCircle(random(0, width), random(0, height));
 
 function loop() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
     ctx.fillRect(0, 0, width, height);
+    let count = 0;
     for (let i = 0; i < balls.length; i++) {
         if (balls[i].exists) {
             balls[i].draw();
             balls[i].update();
             balls[i].collisionDetect();
+        } else {
+            count++;
         }
     }
+    document.getElementsByTagName('p')[0].innerHTML = `Ball count: ${count}`;
     evilBall.draw();
     evilBall.checkBounds();
     evilBall.collisionDetect();
